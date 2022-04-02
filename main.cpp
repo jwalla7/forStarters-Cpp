f37fa965d994cade15e5212e8297b57be25b7d01
Jamal Wallace <j7wallace7@outlook.com>
2022-04-02 03:39:31 -0500

    initial commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+bool isPalindrome(std::string text) {
+    std::string reversed_text = "";
+
+    for (int i = text.size() - 1; i >= 0; i--) {
+        reversed_text += text[i];
+    }
+    
+    if (reversed_text == text) {
+        return true;
+    }
+
+    return false;
+}
+
+int main() {
+    std::cout << isPalindrome("madam") << "\n";
+    std::cout << isPalindrome("racecar") << "\n";
+    std::cout << isPalindrome("matter") << "\n";
+}
