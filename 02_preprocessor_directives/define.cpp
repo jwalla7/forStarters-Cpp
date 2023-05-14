f78997b1c04d13ac3caf50a27f9bbca13b89ad41
jwalla7 <jwalla7@protonmail.com>
2023-05-13 23:14:37 -0500

    0513

---
+++ 02_preprocessor_directives/define.cpp
@@ -0,0 +1,11 @@
+// #define identifier substitution_text
+#include <iostream>
+using namespace std;
+
+#define WORLD_STRING "World"
+
+int main() {
+    cout <<"Hello " << WORLD_STRING << "\n"; // Hello World
+    return 0;
+}
+
