ece945430dd381c5ef07876440e9e4ce0b716d6c
jwalla7 <jwalla7@protonmail.com>
2023-05-31 14:16:25 -0500

    0531

--- 01_basic_syntax/04_vectors.cpp
+++ 01_basic_syntax/04_vectors.cpp
@@ -1,7 +1,7 @@
 /*
 Vectors provide a dynamic array functionality that handles it's own memory management.
-    This enables resizing (adding or removing elements) at runtime without having to explicitly allocate/deallocate memory use the "new", and "delete" operators.
-    (The new operator allocates memory from the "free store", also known as "dynamic memory and heap")
+This enables resizing (adding or removing elements) at runtime without having to explicitly allocate/deallocate memory use the "new", and "delete" operators.
+(The new operator allocates memory from the "free store", also known as "dynamic memory and heap")
 
 https://www.learncpp.com/cpp-tutorial/an-introduction-to-stdvector/
 
@@ -15,13 +15,13 @@ using namespace std;
 vector<int> numbersVector(0);
 
 void addElementToEndOfNumbersVector(int number) {
-    // push_back() function adds element to the back of the numbers vector. push_back function does not replace elements.
+// The push_back() function adds element to the back of the numbers vector, and does not replace elements.
     numbersVector.push_back(number);
     cout << "You added number: " << number << " to the end of numbersVector" << "\n";
 }
 
 void addElementToSpecificIndexOfNumbersVector (int enter_number, int select_index) {
-    // Function inserts new element and shifts the following (elements with index positions after new element) indexed elements;
+// Function inserts new element and shifts the following (elements with index positions after new element) indexed elements;
     // Basic input validation
     if (enter_number < 0 ) {
         throw runtime_error("Invalid, please input a positive integer.");
