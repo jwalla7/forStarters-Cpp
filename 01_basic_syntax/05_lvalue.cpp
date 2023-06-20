5cbe63c4619916339bdf44b003902e5d2a4d1c29
jwalla7 <jwalla7@protonmail.com>
2023-06-20 10:54:54 -0400

    0620

---
+++ 01_basic_syntax/05_lvalue.cpp
@@ -0,0 +1,5 @@
+/*
+References enable you to read or modify the object being referenced. There are two types of references: lvalue and rvalue
+The lvalue is an expression that refers to an object and can refer to a constant. A lvalue that is not declared const is often called a `modifiable lvalue`.
+
+*/
