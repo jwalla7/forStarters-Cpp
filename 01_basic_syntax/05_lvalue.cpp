3edc3759a69a3f4b21ae3629c25fecde33d2bdc5
jwalla7 <jwalla7@protonmail.com>
2023-08-04 11:56:01 -0500

    0804

--- 01_basic_syntax/05_lvalue.cpp
+++ 01_basic_syntax/05_lvalue.cpp
@@ -1,5 +1,29 @@
-/*
-References enable you to read or modify the object being referenced. There are two types of references: lvalue and rvalue
-The lvalue is an expression that refers to an object and can refer to a constant. A lvalue that is not declared const is often called a `modifiable lvalue`.
+/**
+References enable you to read or modify the object being referenced. They are an alias for an existing object. There are two types of references: lvalue and rvalue
+The lvalue is an expression that refers to an object (identity to a specific location in memory) and can refer to a constant. A lvalue that is not declared const is often called a `modifiable lvalue`.
+To declare an lvalue reference type, use an ampersand in the type declaration.
 
+Declarations of reference variables require initialization.
+
+https://www.learncpp.com/cpp-tutorial/lvalue-references/
 */
+#include <iostream>
+
+using namespace std;
+
+int x = 3;
+double y = 5;
+char A = 'A';
+
+int& integer = x; // (&integer) variable is a lvalue to an object
+double& decimal = y; // (&decimal) variable is a lvalue to a double object
+char& character = A; // (&character) variable is a lvalue to a character object
+
+
+int main() {
+    cout << "integer: " << x << "\n";
+    cout << "double: " << y << "\n";
+    cout << "char: " << A << "\n";
+    
+    return 0;
+}
